18ff8fb09d780118bbbd4ffb65ec5f3914bca484
Unknown <adilabbas6@yahoo.com>
2018-10-29 10:55:19 -0400

    v1
    -added Whack-A-Mole

---
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/WhackAMole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class WhackAMole : CarnivalGame
+    {
+        public WhackAMole() : base()
+        {
+            
+        }
+
+        public override string getName()
+        {
+            return "Whack-A-Mole";
+        }
+        
+        public override void play()
+        {
+            
+        }
+
+    }
+}
