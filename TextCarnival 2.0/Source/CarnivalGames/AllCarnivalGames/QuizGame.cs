47b21b4d0fab92d2fd281413719b5a6653606de1
Unknown <reevesbrehm@gmail.com>
2018-10-29 11:25:09 -0400

    created game

---
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/QuizGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class QuizGame : CarnivalGame
+    {
+        public QuizGame() : base()
+        {
+            
+        }
+
+        public override string getName()
+        {
+            return "The Quiz Game";
+        }
+        
+        public override void play()
+        {
+            writeOut("Welcome to the Random Knowledge Quiz !");
+            writeOut("What is your name ?");
+            String name = getInput();
+            writeOut("Oh Hello " + name + " are you ready to test your knowledge");
+            bool getsIt = getYesNo();
+
+            if (getsIt)
+                writeLine("Great! Best of luck to you");
+            else
+                writeLine("Well looks like your doing it anyway, I bet you fail");
+        }
+
+    }
+}
