063b96d7fef76382a775d6186fddc7763f5399e9
Unknown <adilabbas6@yahoo.com>
2018-10-29 11:33:32 -0400

    v1.1
    -created tutorial
    -added game to project file

--- TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/WhackAMole.cs
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/WhackAMole.cs
@@ -20,8 +20,38 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         
         public override void play()
         {
-            
+            tutorial();
         }
+        public void tutorial()
+        {
+            clear();
+            writeOut("Welcome!"); wait(1);
+            writeOut("This is Whack-A-Mole!"); wait(1);
+            writeOut("In this game, you whack..."); dramaticPause(3);
+            writeOut("...moles!"); wait(1);
+            writeOut("The board looks like this:");
+            writeLine("+ - +  + - +  + - +  + - +");
+            writeLine("- Q -  - W -  - E -  - R -");
+            writeLine("+ - +  + - +  + - +  + - +");
+            writeLine("- A -  - S -  - D -  - F -");
+            writeLine("+ - +  + - +  + - +  + - +");
+            writeLine("- Z -  - X -  - C -  - V -");
+            writeLine("+ - +  + - +  + - +  + - +"); wait(3);
+            writeOut("It's a 4x3 grid, and you hit a mole by hitting the key shown in the box."); wait(1);
+            writeOut("Simple, isn't it?"); wait(1);
+            writeOut("Would you like to see the tutorial again?");
+            bool again = getYesNo();
 
+            if (again)
+            {
+                clear();
+                tutorial();
+            }
+            else
+            {
+                clear();
+                writeOut("Alright, let's start!"); wait(2);
+            }
+        }
     }
 }
