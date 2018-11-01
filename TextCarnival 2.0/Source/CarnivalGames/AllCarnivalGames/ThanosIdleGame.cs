602e26a43ed41b6ef293f9acbe43ce19886992b8
Jason Lima Reyes <ladiesman6974@gmail.com>
2018-11-01 18:58:24 -0400

    Thanos Idle Game

---
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class ThanosIdleGame : CarnivalGame
+    {
+        public ThanosIdleGame() : base()
+        {
+            
+        }
+
+        public override string getName()
+        {
+            return "The Thanos Idle Game";
+        }
+        
+        public override void play()
+        {
+            //Shows the green title text
+            showTitle("WELCOME TO THE THANOS IDLE GAME!");
+            wait(0.5);
+            writeLine("So basically... im monky");
+            clear();
+        }
+
+    }
+}
