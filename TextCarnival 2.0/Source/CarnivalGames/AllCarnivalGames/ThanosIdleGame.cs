f0e79c05cff1761101d226c78d7d30cc945f9b2f
Jason Lima Reyes <ladiesman6974@gmail.com>
2018-11-05 18:40:14 -0500

    Thanos Idle Game v0.3
    -"Now we are really getting somewhere"

--- TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
@@ -22,23 +22,41 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         public override void play()
         {
             int thanoscoins = 0;
-            int increment = 0;
+            int increment = 1;
+            int infinitystonemult = 1;
             int earned;
             int time = 1;
-            String input;
-            //Shows the green title text
+            int thots = 1;
+            
             showTitle("WELCOME TO THE THANOS IDLE GAME!");
             writeLine("So basically... im monky");
             wait(1);
             clear();
+            writeLine("You are the Mad Titan Thanos so click spacebar to raid a world and collect some(or one for now) thanos coins");
+            writeLine("When you are ready my lord access the shop via the X key on your magic keyboard thingy the buy better minions or even \nsome magic stones O-O");
             for (int x = 0; x < time; x = x + 1)
             {
-                input = Console.ReadLine();
-                if(input != " ")
+                ConsoleKeyInfo input = Console.ReadKey();
+                if (input.KeyChar == ' ')
                 {
-                    Console.WriteLine("Should've aimed for the head...(try spacebar UwU)");
+                    clear();
+                    earned = (thanoscoins * -1);
+                    thanoscoins = thanoscoins + (increment * infinitystonemult);
+                    earned = earned + thanoscoins;
+                    writeLine("Lord Thanos you earned " + earned + " that raid...");
+                    writeLine(" Lord Thanos you have " + thanoscoins + " Thanos coins...");
                 }
-                Console.WriteLine("You earned " + " Thanos buck(s)");
+                if (input.KeyChar == 'x')
+                {
+                    clear();
+                    Console.WriteLine("Welcome to the shop Lord Thanos, dont ask me why you of all people have to pay for things...");
+                }
+                else if(input.KeyChar != ' ')
+                {
+                    clear();
+                    writeLine("Try using a key with an actual function\nControls:\nRaid Spacebar\nShop X key");
+                }
+                time = time + 1;
             }
         }
         
