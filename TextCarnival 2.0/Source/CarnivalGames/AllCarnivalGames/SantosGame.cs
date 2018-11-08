30160bcc1e3ac21f8b12f3a2e8e21a608f1d71e9
Unknown <4surferwahine.bp@gmail.com>
2018-11-08 12:57:18 -0500

    did more work!!!

--- TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/SantosGame.cs
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/SantosGame.cs
@@ -15,147 +15,108 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
 
         public override string getName()
         {
-            return "The Santos Example Game";
+            return "MadLibs";
         }
         
         public override void play()
         {
             //Shows the green title text
-            showTitle("WELCOME TO THE EXAMPLE GAME!");
-
-            writeLine("This is a basic tutorial on how to use this text basted engine and make games");
-            writeLine("Follow along with it both in the console and in the code to see what to do");
-
-            //Color example
-            writeLine("|70Cool that I can color things right?|f0");
-
-            //To add color put the character ' | ' somewhere in a string
-            //then follow it by two hexadecimal characters that represend color
-            //|a0 would represend changing the string to foreground color 'a' and background color '0'
-            //Experiment to see what characters give what color!
-            //black is '0' and white is 'f'
-            //this is not part of normal c#, the client side of this program actualy parses the string and changes color
-            //On a normal c# project this will not work
-            writeLine("|50The Code shows how to do this!|f0");
-            
-            //Shows a seperation 
-            showSeperator(30);
-
-            writeLine("|e0Hey, yea you, are you getting this so far?");
-
-            //Asks for a yes or no responce and return if it was yes
-            bool getsIt = getYesNo();
-
-            if (getsIt)
-                writeLine("Cool, you get it dont you!");
-            else
-                writeLine("You dont :( how sad, look at the code then.");
-
-            writeLine("Try not answering yes or no to this next one, see how that goes.");
-            writeLine("Are you a enjoying this so far?");
-
-            //Gets yes / no
-            bool likesIt = getYesNo();
-
-            writeLine("You see how you can controll what the user types so that it has to be something like I requested");
-
-            writeLine("It doesnt have to be yes or no either.");
-
-            writeLine("Are you a [boy] or a [girl]?");      //OMG dont get triggered, its an example
-
-            //Gets a option
-            String gender = getOption("boy", "girl");       //YES! in this case there are two options, this is for learning
-
-            writeLine("See there how I restricted it to whatever I wanted, not just yes or no?");
-
-            wait(0.5);
-
-            //Writes the line, character by character
-            writeOut("Hey! look at this line, it is writing itself, isnt that cool!");
-            writeOut("TEST!");
-            writeOut("TEST!");
-            writeOut("TEST!");
-
-            writeOut("cool right!");
-
-            getYesNo();
-        
-            //Clears console
             clear();
+            showTitle("Welcome to MadLibs!");
 
-            writeOut("You can also clear the screen like i just did!");
+            writeLine("Time to write your own story.");
+            writeLine("Choose from one of the genres listed:");
 
-            wait(0.5);
+            writeOut("Rom Com");
+            String input = getInput();
 
-            writeOut("You can also have the user wait for something, or for a specific amount of time");
+            if (input.Equals("rom com"))
+            {
+                writeOut("I need 14 words from you to finish the story--I'll ask them for you now, one by one.");
+                writeLine("");
 
-            //Waits x amoutn of seconds, doesnt have to be a whole number
-            wait(3.1);
+                writeOut("Name (your own, or a friend's, if you would rather subject them to the happenings of the cheesy plot line): ");
+                String name = getInput();
+                writeLine("");
 
-            writeOut("That was 3 seconds");
+                writeOut("A friend's name: ");
+                String fName = getInput();
+                writeLine("");
 
-            wait(2);
+                writeOut("A nice, generic boy name: ");
+                String boyName = getInput();
+                writeLine("");
 
-            writeOut("And that was another 2");
+                writeOut("And a nice, equally generic girl name: ");
+                String girlName = getInput();
+                writeLine("");
 
-            writeOut("You can even make it super dramatic if you want");
+                writeOut("Two verbs ending in -ing: ");
+                String ing1 = getInput();
+                String ing2 = getInput();
+                writeLine("");
 
-            //The number specifies how many seconds it waits
-            dramaticPause(3);
+                writeOut("A verb ending in -ed: ");
+                String ed1 = getInput();
+                writeLine("");
 
-            writeOut("Do you get it so far?");
+                writeOut("A noun: ");
+                String noun = getInput();
+                writeLine("");
 
-            //Gets yes no
-            bool stillGetsIt = getYesNo();
+                writeOut("Two adjectives: ");
+                String adj1 = getInput();
+                String adj2 = getInput();
+                writeLine("");
 
-            //YOu can use custom colors
-            showSeperator(30,"|e0","|d0");
+                writeOut("Two exclamations: ");
+                String exc = getInput();
+                String exc2 = getInput();
+                writeLine("");
 
-            writeOut("Oh, by the way, whats your name");
+                writeOut("A plural noun: ");
+                String pluNoun = getInput();
+                writeLine("");
 
-            String name = getInput();
+                write("Finally, a plural body part: ");
+                String bodyPart = getInput();
+                writeLine("");
 
-            writeOut("See, " + name + " this can also get raw inputs too");
+                // Transition
+                writeLine("Wonderful. Time for your final product :)");
+                clear();
+                dramaticPause(3);
 
-            wait(1);
+                writeOut(name + " wasn't like other girls.");
+                writeLine("");
 
-            writeOut("If you want you can also make the console play sounds");
+                writeOut("Most high schoolers didn't care about their grades--");
+                dramaticPause(1);
+                writeOut(name + " absolutely lost her mind for academics.");
+                writeLine("");
 
-            //Makes the console beep
-            beep();
+                writeOut("Most high schoolers went to football games and parties--");
+                dramaticPause(1);
+                writeOut(name + " found comfort  in " + ing1 + " in the peace and comfort of her own home.");
+                writeLine("");
 
-            writeLine("They are fairly basic, but you can specify frequency and durration if you want");
+                writeOut("And most high schoolers had a guy or girl to call their own, their " + noun + "ipoo--");
+                dramaticPause(1);
+                writeOut(name + " was extremely, extremely single.");
+                writeLine("");
 
-            //Using the beep command you can specify frequency and durration
-            beep(700, 2);
-            beep(800, 0.5f);
-            beep(900, 0.4f);
-            beep(600, 1);
+                dramaticPause(2);
+                writeOut("But you can imagine that changed nice and quick.");
+                writeLine("");
 
-            writeLine("Thats kinda cool, right!");
-            writeLine("If you combine some of these features you can do things like this!");
-            writeLine("Wana see?");
-            getYesNo();
+                writeOut("Most predictably, " + name + " was just " + ing2 + " in the library one " + adj1 + " Tuesday afternoon. " +
+                    "\nShe always did this with the most extreme focus. Suddenly, to her great surprise,\n the second bell for class was already ringing. \"" +
+                    exc + "!\" She yelled. \n\" 1:00 already???\" She picked up all her " + pluNoun + " and bolted out the door, rounding corners like a Tokyo drifter." +
+                    "But alas,\nshe wasn't slick enough to avoid bumping " + bodyPart + " with the most popular guy in school.");
 
-            //This prints out text to try to sound like a type writer
-            typeWriter("This is very slow, but kinda cool!");
-
-            wait(1);
-
-            writeLine("Thats about all that this can do.");
-            
-            writeLine("Have fun and build an amazing game!");
-
-            // To make your own game, dont edit this, but rather create another file in this same locaiton
-            // Name the class whatever you want, but make sure that you extend the main carnival game class (inheritance)
-            //
-            // class [your class name] : CarnivalGame
-            //
-            // Make sure its namespace at the top reads:
-            //
-            // namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
-            //
-            // Have fun
+                writeOut("\"" + exc2 + "\" " + name + " yelled as she tumbled to the floor with her " + pluNoun + 
+                    );
+            }
 
         }
 
