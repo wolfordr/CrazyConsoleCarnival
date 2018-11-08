c8c70e977e29ffb2a13fc01f6c370f9d48076a30
Jason Lima Reyes <ladiesman6974@gmail.com>
2018-11-07 22:31:14 -0500

    Thanos Idle Game v0.5
    -earning completed(for the most part)
    -infinity stone shop added
    -thot shop function a success
    -fixed variable reset bug

--- TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
@@ -18,17 +18,18 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         {
             return "The Thanos Idle Game";
         }
-        
+
+        int thanoscoins { get; set; }
+        int planetRaid { get; set; }
+        int minions { get; set; }
+        int infinitystonemult { get; set; }
+        int fleet { get; set; }
+        int earned { get; set; }
+        int time { get; set; }
+        int thots { get; set; }
+
         public override void play()
         {
-            int thanoscoins = 0;
-            int planetRaid = 1;
-            int minions = 0;
-            int infinitystonemult = 1;
-            int fleet = 1;
-            int earned;
-            int time = 1;
-            int thots = 1;
             
             showTitle("WELCOME TO THE THANOS IDLE GAME!");
             writeLine("So basically... im monky");
@@ -36,10 +37,21 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             clear();
             writeLine("You are the Mad Titan Thanos so click spacebar to raid a world and collect some(or one for now) thanos coins");
             writeLine("When you are ready my lord access the shop via the X key on your magic keyboard thingy to buy better minions or even \nsome magic stones O-O");
-            for (int x = 0; x < time; x = x + 1)
+            int x = 0;
+            if (x == 0)
+            {
+                thanoscoins = 0;
+                planetRaid = 1;
+                minions = 0;
+                infinitystonemult = 1;
+                fleet = 1;
+                time = 1;
+                thots = 1;
+            }
+            for (x = 0; x < time; x = x + 1)
             {
                 ConsoleKeyInfo input = Console.ReadKey();
-                wait(.2);
+                
                 if (input.KeyChar == ' ')
                 {
                     clear();
@@ -79,6 +91,88 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             {
                 clear();
                 writeLine("Infinity Stones are objects which hold great power over an aspect of the universe and they add to your stone multiplier\nBuy Power for 5000[A]\nBuy Space for 20000[D]\nBuy the Reality for 50000[F]\nBuy the Soul Stone for 150000[G]\nBuy the Time Stone for 250000[H]\nBuy The Mind Stone for 1000000[J]");
+                ConsoleKeyInfo input2 = Console.ReadKey();
+                if(input2.KeyChar == 'a')
+                {
+                    if(thanoscoins >= 5000)
+                    {
+                        clear();
+                        thanoscoins = (thanoscoins - 5000);
+                        infinitystonemult = infinitystonemult + 1;
+                        writeLine("You bought the Orb!");
+                    }
+                }
+                if (input2.KeyChar == 's')
+                {
+                    if (thanoscoins >= 20000)
+                    {
+                        clear();
+                        thanoscoins = (thanoscoins - 20000);
+                        infinitystonemult = infinitystonemult + 1;
+                        writeLine("You bought the Tesseract!");
+                    }
+                }
+                if (input2.KeyChar == 'd')
+                {
+                    if (thanoscoins >= 50000)
+                    {
+                        clear();
+                        thanoscoins = (thanoscoins - 50000);
+                        infinitystonemult = infinitystonemult + 1;
+                        writeLine("You bought the Aether!");
+                    }
+                }
+                if (input2.KeyChar == 'f')
+                {
+                    if (thanoscoins >= 5000)
+                    {
+                        clear();
+                        thanoscoins = (thanoscoins - 5000);
+                        infinitystonemult = infinitystonemult + 1;
+                        writeLine("You bought the stone of power!");
+                    }
+                }
+                if (input2.KeyChar == 'g')
+                {
+                    if (thanoscoins >= 150000)
+                    {
+                        clear();
+                        writeLine("You actually cannot buy this one my lord...");
+                        wait(3);
+                        writeLine("You must sacrifice something close to you my lord...");
+                        wait(3);
+                        writeLine("Kill Gamora? Y/N");
+                        bool answer = getYesNo();
+                        if(answer)
+                        {
+                            writeLine("The number of thots on this planet: " + thots);
+                        }
+                        if(!answer)
+                        {
+                            writeLine("Don't do that, thots are temporary, infinity stones are forever.\nTry again later");
+                        }
+                    }
+                }
+                if (input2.KeyChar == 'h')
+                {
+                    if (thanoscoins >= 250000)
+                    {
+                        clear();
+                        thanoscoins = (thanoscoins - 250000);
+                        infinitystonemult = infinitystonemult + 1;
+                        writeLine("You bought the Eye of Agamoto!");
+                    }
+                }
+                if (input2.KeyChar == 'j')
+                {
+                    if (thanoscoins >= 1000000)
+                    {
+                        clear();
+                        thanoscoins = (thanoscoins - 5000);
+                        infinitystonemult = infinitystonemult + 1;
+                        writeLine("You freakin killed vision dude holy moly!!!111!1!!!!!!");
+                    }
+                }
             }
             if(input.KeyChar == 's')
             {
@@ -94,6 +188,11 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             {
                 clear();
                 writeLine("Would you like to know how many thots there are? Y/N");
+                bool thotanswer = getYesNo();
+                if(thotanswer)
+                {
+                    writeLine("There are " + thots + " thots in this sector sir.");
+                }
             }
         }
         
