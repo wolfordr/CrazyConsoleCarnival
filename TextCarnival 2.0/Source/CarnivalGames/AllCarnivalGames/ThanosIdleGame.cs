91982f69f205271e37345972050c3fe734208bf9
Jason Lima Reyes <ladiesman6974@gmail.com>
2018-11-08 22:33:58 -0500

    Thanos Idle Game v0.7.1
    -Finishing up shop functions mainly rejection messages

--- TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ThanosIdleGame.cs
@@ -101,6 +101,11 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                         infinitystonemult = infinitystonemult + 1;
                         writeLine("You bought the Orb!");
                     }
+                    else
+                    {
+                        clear();
+                        writeLine("You cannot afford that my lord! Please dont hurt me...");
+                    }
                 }
                 if (inputStone == ConsoleKey.S)
                 {
@@ -110,6 +115,11 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                         thanoscoins = (thanoscoins - 20000);
                         infinitystonemult = infinitystonemult + 1;
                         writeLine("You bought the Tesseract!");
+                    }
+                    else
+                    {
+                        clear();
+                        writeLine("You cannot afford that my lord! Please dont hurt me...");
                     }
                 }
                 if (inputStone == ConsoleKey.D)
@@ -121,6 +131,11 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                         infinitystonemult = infinitystonemult + 1;
                         writeLine("You bought the Aether!");
                     }
+                    else
+                    {
+                        clear();
+                        writeLine("You cannot afford that my lord! Please dont hurt me...");
+                    }
                 }
                 if (inputStone == ConsoleKey.F)
                 {
@@ -130,6 +145,11 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                         thanoscoins = (thanoscoins - 5000);
                         infinitystonemult = infinitystonemult + 1;
                         writeLine("You bought the stone of power!");
+                    }
+                    else
+                    {
+                        clear();
+                        writeLine("You cannot afford that my lord! Please dont hurt me...");
                     }
                 }
                 if (inputStone == ConsoleKey.G)
