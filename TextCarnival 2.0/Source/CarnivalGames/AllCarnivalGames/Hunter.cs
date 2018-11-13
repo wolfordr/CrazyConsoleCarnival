96cc0c802995e95ecf134915baa6d54a7f1dcddf
Unknown <hdagon01@gmail.com>
2018-11-13 11:33:50 -0500

    Progressed story, added one ending.

--- TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Hunter.cs
+++ TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Hunter.cs
@@ -41,9 +41,33 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                     writeLIne("You reluctantly accept, starting your walk towards the house.");
                     //wait()
                     writeLine("Carl opens the front door of Old Man Jenkins' house and you both walk in together.");
-                    writeLIne("The house appears as though it hasn't been inhabited decades.");
-                    writeLIne("Everything is covered in a thick layer of dust and cobwebs.");
-                    writeLIne("");
+                    writeLine("The house appears as though it hasn't been inhabited decades.");
+                    writeLine("Everything is covered in a thick layer of dust and cobwebs.");
+                    writeLine("Carl - Let's go find Old Man Jenkins!");
+                    writeLine("Carl sprints up a staircase and zips out of sight.");
+                    writeLine("You weren't that interested to begin with so you just walk after him.");
+                    writeLine("Walking up the stairs, you notice old portraits of family hanging on the walls.");
+                    writeLine("You reach the top of the stair case and arrive at a long hallway.");
+                    writeLine("It's so dark that you can't see the end of the hallway");
+                    writeLine("You...");
+                    writeLine("1. Run towards the scream to help your friend.");
+                    writeLine("2. Turn around and walk away. You didn't like Carl that much anyways.");
+                    if(getKey() == ConsoleKey.NumPad1)
+                    {
+                        
+                    }
+                    if (getKey() == ConsoleKey.NumPad2)
+                    {
+                        clear();
+                        writeLine("You start down the stairs, but you hear heavy footsteps coming quickly down the hallway.");
+                        writeLine("You quicken your pace and head for the door.");
+                        writeLine("You make it to the door, but the handle won't budge. You're trapped.");
+                        writeLine("You turn around to find Carl right infront of you holding a large knife.");
+                        writeLine("His eyes are rolled back so only the white show, and his neck is red and bloody.");
+                        writeLine("Carl plunges his knife deep into your chest without warning.");
+                        writeLine("Everything goes black. you have died.");
+                        writeLine("GAME OVER");
+                    }
                 }
 
 
